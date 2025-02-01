c49593238f1c49f2b5df6a0a0b60de830e0fe113
atsekhan <atsekhan@gmail.com>
2025-01-31 19:34:55 -0500

    added SmartDashboard for CANRange

--- C2025_release/src/main/java/frc/robot/subsystems/SmartDashboardSubsystem.java
+++ C2025_release/src/main/java/frc/robot/subsystems/SmartDashboardSubsystem.java
@@ -77,7 +77,10 @@ public class SmartDashboardSubsystem extends SubsystemBase {
     if (DebugTelemetrySubsystems.climber) {
       updateClimberTelemetry();
     }
-    updateIntakeTelemetry();
+    if(DebugTelemetrySubsystems.intake) {
+      updateIntakeTelemetry();
+    }
+    
   }
 
   @Override
