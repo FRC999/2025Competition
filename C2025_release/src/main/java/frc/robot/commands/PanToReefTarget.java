6005be1aa854bb1643bd9f1a6a73977718a0bd80
atsekhan <atsekhan@gmail.com>
2025-02-03 19:34:20 -0500

    add distance tolerance to isFinished in PanToReefTarget

--- C2025_release/src/main/java/frc/robot/commands/PanToReefTarget.java
+++ C2025_release/src/main/java/frc/robot/commands/PanToReefTarget.java
@@ -6,6 +6,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.RobotContainer;
+import frc.robot.Constants.GPMConstants.IntakeConstants.ReefFinderConstants;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class PanToReefTarget extends Command {
@@ -21,6 +22,7 @@ public class PanToReefTarget extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    System.out.println("*** PanToReefTarget command started ");
     RobotContainer.driveSubsystem.drive(0, setYVelocity, 0);
   }
 
@@ -30,11 +32,14 @@ public class PanToReefTarget extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    System.out.println("*** PanToReefTarget command ended " + interrupted);
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return RobotContainer.reefFinderSubsystem.isTargetVisible();
+    return RobotContainer.reefFinderSubsystem.isTargetVisible() && 
+      RobotContainer.reefFinderSubsystem.getDistanceToTarget()<=ReefFinderConstants.minDistanceToTarget;
   }
 }
