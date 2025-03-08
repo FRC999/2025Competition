bc3ba91de5d40a1669b4fa1bca504210b41252fc
Sudhamsu <academyoffice79@gmail.com>
2025-03-07 21:23:44 -0500

    path planner auto fix and addition of coral cruise method on auto routine

--- C2025_release/src/main/java/frc/robot/Robot.java
+++ C2025_release/src/main/java/frc/robot/Robot.java
@@ -71,6 +71,8 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
     }
+    RobotContainer.armSubsystem.setArmPositionWithAngle(ArmPositions.CoralCruise);
+    RobotContainer.setIfAllianceRed();
   }
 
   @Override
