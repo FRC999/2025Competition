bba74783389e6012604c893813524f52a6991e5d
atsekhan <atsekhan@gmail.com>
2025-03-08 19:28:35 -0500

    Red one coral l4 addition

---
+++ C2025_release/src/main/java/frc/robot/commands/AutoRed1Coral.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.RobotContainer;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class AutoRed1Coral extends SequentialCommandGroup {
+  /** Creates a new AutoRed1Coral. */
+  public AutoRed1Coral() {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+      new InstantCommand( ()-> RobotContainer.driveSubsystem.setOdometryToIdealPoseFromTrajectory("Red-BargeToReef1"))
+        // just in case - wait 0.1s for the pose to take place
+        .andThen(new WaitCommand(0.1))
+        .andThen(RobotContainer.runTrajectoryPathPlannerWithForceResetOfStartingPose("Red-BargeToReef1", true,false))
+        .andThen(new WaitCommand(0.1))
+        .andThen(new TeleopMoveToL4RotateArm()) 
+        .andThen(new TeleopEjectCoralBringArmToCruiseElevatorDown())
+    );
+  }
+}
