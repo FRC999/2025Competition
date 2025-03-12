d02abc2a7f2489089e74f1d3d8520fb8395c54d5
Alex Tsekhansky <atsekhan@gmail.com>
2025-03-12 09:46:54 -0400

    get vision-aided odometry pose

--- C2025_release/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ C2025_release/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -576,72 +576,79 @@ public class DriveSubsystem extends SwerveDrivetrain<TalonFX,TalonFX,CANcoder> i
   }
 
   private void configureAutoBuilder() {
-        try {
-            var config = RobotConfig.fromGUISettings();
-            AutoBuilder.configure(
-                () -> getState().Pose,   // Supplier of current robot pose
-                this::resetPose,         // Consumer for seeding pose against auto
-                () -> getState().Speeds, // Supplier of current robot speeds
-                // Consumer of ChassisSpeeds and feedforwards to drive the robot
-                (speeds, feedforwards) -> setControl(
-                    m_pathApplyRobotSpeeds.withSpeeds(speeds)
-                        .withWheelForceFeedforwardsX(feedforwards.robotRelativeForcesXNewtons())
-                        .withWheelForceFeedforwardsY(feedforwards.robotRelativeForcesYNewtons())
-                ),
-                new PPHolonomicDriveController(
-                    // PID constants for translation
-                    new PIDConstants(10, 0, 0),
-                    // PID constants for rotation
-                    new PIDConstants(7, 0, 0)
-                ),
-                config,
-                // Assume the path needs to be flipped for Red vs Blue, this is normally the case
-                () -> DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red,
-                this // Subsystem for requirements
-            );
-        } catch (Exception ex) {
-            DriverStation.reportError("Failed to load PathPlanner config and configure AutoBuilder", ex.getStackTrace());
-        }
+    try {
+      var config = RobotConfig.fromGUISettings();
+      AutoBuilder.configure(
+          () -> getState().Pose, // Supplier of current robot pose
+          this::resetPose, // Consumer for seeding pose against auto
+          () -> getState().Speeds, // Supplier of current robot speeds
+          // Consumer of ChassisSpeeds and feedforwards to drive the robot
+          (speeds, feedforwards) -> setControl(
+              m_pathApplyRobotSpeeds.withSpeeds(speeds)
+                  .withWheelForceFeedforwardsX(feedforwards.robotRelativeForcesXNewtons())
+                  .withWheelForceFeedforwardsY(feedforwards.robotRelativeForcesYNewtons())),
+          new PPHolonomicDriveController(
+              // PID constants for translation
+              new PIDConstants(10, 0, 0),
+              // PID constants for rotation
+              new PIDConstants(7, 0, 0)),
+          config,
+          // Assume the path needs to be flipped for Red vs Blue, this is normally the
+          // case
+          () -> DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red,
+          this // Subsystem for requirements
+      );
+    } catch (Exception ex) {
+      DriverStation.reportError("Failed to load PathPlanner config and configure AutoBuilder", ex.getStackTrace());
     }
+  }
 
-    public void setRobotCentricTrue() {
-      isRobotCentric = true; 
+  public void setRobotCentricTrue() {
+    isRobotCentric = true;
+  }
+
+  public void setRobotCentricFalse() {
+    isRobotCentric = false;
+  }
+
+  public boolean getRobotCentric() {
+    return isRobotCentric;
+  }
+
+  public void setOdometryPoseToSpecificPose(Pose2d p) {
+    this.resetPose(p);
+  }
+
+  /**
+   * Take trajectory, extract starting pose, reset odometry to it
+   * 
+   * @param tr
+   */
+  public void setOdometryToIdealPoseFromTrajectory(String tr) {
+    try {
+      // Load the path you want to follow using its name in the GUI
+      PathPlannerPath path = PathPlannerPath.fromPathFile(tr);
+
+      Pose2d startPose = path.getStartingHolonomicPose().get();
+      setOdometryPoseToSpecificPose(startPose); // reset odometry, as PP may not do so
+    } catch (Exception e) {
+      DriverStation.reportError("Big oops: " + e.getMessage(), e.getStackTrace());
     }
+  }
 
-    public void setRobotCentricFalse() {
-      isRobotCentric = false; 
+  public void setCurrentOdometryPoseToSpecificRotation(double degrees) {
+    Pose2d currentPose = this.getPose();
+    Pose2d newPose = new Pose2d(currentPose.getX(), currentPose.getY(), Rotation2d.fromDegrees(degrees));
+    setOdometryPoseToSpecificPose(newPose);
+  }
+
+  public Pose2d getVisionAidedOdometryPose() {
+    if (RobotContainer.llVisionSubsystem.isAprilTagVisibleBySomeCamera()) {
+      return RobotContainer.llVisionSubsystem.getBestPoseAllCameras();
+    } else {
+      return getPose();
     }
-
-    public boolean getRobotCentric() {
-      return isRobotCentric; 
-    }
-
-    public void setOdometryPoseToSpecificPose(Pose2d p) {
-      this.resetPose(p);
-    }
-
-    /**
-     * Take trajectory, extract starting pose, reset odometry to it
-     * @param tr
-     */
-    public void setOdometryToIdealPoseFromTrajectory(String tr) {
-      try {
-        // Load the path you want to follow using its name in the GUI
-        PathPlannerPath path = PathPlannerPath.fromPathFile(tr);
-
-        Pose2d startPose = path.getStartingHolonomicPose().get();
-        setOdometryPoseToSpecificPose(startPose); // reset odometry, as PP may not do so
-      } catch (Exception e) {
-        DriverStation.reportError("Big oops: " + e.getMessage(), e.getStackTrace());
-      }
-    }
-
-    public void setCurrentOdometryPoseToSpecificRotation(double degrees) {
-      Pose2d currentPose = this.getPose();
-      Pose2d newPose = new Pose2d(currentPose.getX(), currentPose.getY(), Rotation2d.fromDegrees(degrees));
-      setOdometryPoseToSpecificPose(newPose);
-    }
-
+  }
 
   @Override
   public void periodic() {
