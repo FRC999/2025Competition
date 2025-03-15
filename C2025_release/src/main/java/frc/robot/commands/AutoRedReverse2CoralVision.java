5780f1511ce2e267c5959d34ded50f631a4ce629
atsekhan <atsekhan@gmail.com>
2025-03-15 14:52:14 -0400

    changes were made

--- C2025_release/src/main/java/frc/robot/commands/AutoRedReverse2CoralVision.java
+++ C2025_release/src/main/java/frc/robot/commands/AutoRedReverse2CoralVision.java
@@ -29,13 +29,13 @@ public class AutoRedReverse2CoralVision extends SequentialCommandGroup {
 
         new DeferredCommand(
             () -> new SetOdometryToVisionPose()
-               .andThen(new PrintCommand(
-                   "---A From: " + RobotContainer.driveSubsystem.getInitialVisionAidedOdometryPose(new Pose2d(10.331, 6.130, Rotation2d.kZero)) +
-                       " To: " + RobotPoseConstants.visionRobotPoses.get("Red-BargeToReef3").toString()))
+            //    .andThen(new PrintCommand(
+            //        "---A From: " + RobotContainer.driveSubsystem.getInitialVisionAidedOdometryPose(new Pose2d(10.331, 6.130, Rotation2d.kZero)) +
+            //            " To: " + RobotPoseConstants.visionRobotPoses.get("Red-BargeToReef3").toString()))
                         ,
             Set.of()),
             RobotContainer.runTrajectoryPathPlannerWithForceResetOfStartingPose(
-                "Red-BargeToReef3", false, false),
+                "Red-BargeToReef3", true, false),
         // new DeferredCommand(
         //     () -> RobotContainer.runTrajectory2PosesSlow(
         //         RobotContainer.driveSubsystem.getInitialVisionAidedOdometryPose( new Pose2d(7.219, 6.130, Rotation2d.k180deg)), // if vision is not available at the start, use that pose
